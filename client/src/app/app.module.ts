995abf806c5cc58b89637036a1957bfa68f6ce8e
DJuanmix <juanmaortizsisamon@gmail.com>
2018-08-05 21:41:38 +0200

    Generaing new components and routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/common/login/login.component';
+import { MenuComponent } from './components/common/menu/menu.component';
+import { DashboardComponent } from './components/mobile/dashboard/dashboard.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		LoginComponent,
+		MenuComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule
